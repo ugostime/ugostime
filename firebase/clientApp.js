f9487836c84b2264eccca63729388f8bc86d84cf
ugostime <ugosti.me.solutions@gmail.com>
2021-10-28 12:05:30 +0200

    nadogradnja 5

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import * as firebase  from "firebase/app";
+import * as firebase  from "firebase";
 import {getAuth} from "firebase/auth";
 //import { getApps } from "firebase/app";
 //import  "firebase/firestore";
